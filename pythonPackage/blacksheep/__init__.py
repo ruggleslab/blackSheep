0870e91b708039e97fc5ef1a7e8f83908dfdd530
Lili Blumenberg <lili.blumenberg@gmail.com>
2019-07-31 15:54:52 -0400

    adding test brca retro data

--- pythonPackage/blacksheep/__init__.py
+++ pythonPackage/blacksheep/__init__.py
@@ -4,6 +4,14 @@ from .visualization import plot_heatmap
 from .parsers import binarize_annotations
 
 
+fmt = '%(asctime)s:%(levelname)s:%(message)s'
+logging.basicConfig(
+        format=fmt,
+        level=logging.INFO,
+        datefmt='%m/%d/%Y %H:%M:%S'
+    )
+
+
 __all__ = [
     "make_outliers_table",
     "compare_groups_outliers",
