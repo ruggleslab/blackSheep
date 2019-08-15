14d8a2b4737db7e6fc9a18eb6719302e6a697e36
Lili Blumenberg <lili.blumenberg@gmail.com>
2019-08-15 16:11:51 -0400

    removing old htmls and maybe fixing conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
-
+master_doc="index"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
